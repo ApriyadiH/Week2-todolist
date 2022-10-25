eb302f5421f79aee63cfc7312e03a380eed94e0e
Apriyadi Hegarto <ahegarto@gmail.com>
2022-10-25 21:41:52 +0700

    interface selesai

--- src/App.js
+++ src/App.js
@@ -104,13 +104,9 @@ const ToDoWorking = () => {
         display: "flex",
         flexWrap: "wrap",
         gap: "12px",
-        flexDirection: "column",
-        justifyContent: "center"
     }
 
     const to_do_container = {
-        display: "flex",
-        gap: "12px",
         border: "4px solid teal",
         borderRadius: "12px",
         padding: "12px 24px 24px",
@@ -130,13 +126,12 @@ const ToDoWorking = () => {
     const button_set = {
         display: "flex",
         gap: "10px",
-        marginTop: "24px"
+        marginTop: "24px",      
       }
 
     const delete_button = {
         backgroundColor: "#fff",
         border: "2px solid red",
-        border: "none",
         borderRadius: "8px",
         cursor: "pointer",
         height: "40px",
@@ -145,7 +140,11 @@ const ToDoWorking = () => {
 
     const complete_button = {
         backgroundColor: "#fff",
-        border: "2px solid green"
+        border: "2px solid green",
+        borderRadius: "8px",
+        cursor: "pointer",
+        height: "40px",
+        width: "50%"
     }
 
     return (
@@ -160,6 +159,130 @@ const ToDoWorking = () => {
                     <button style = {complete_button}>Done</button>
                 </div>
             </div>
+            {/* <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div>
+            <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div>
+            <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div> */}
+        </div>
+    )
+}
+
+const ToDoComplete = () => {
+    const list_wrapper = {
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "12px",
+    }
+
+    const to_do_container = {
+        border: "4px solid teal",
+        borderRadius: "12px",
+        padding: "12px 24px 24px",
+        width: "270px"
+    }
+
+    const to_do_title = {
+        display: "block",
+        fontSize: "1.5em",
+        marginBlockStart: "0.83em",
+        marginBlockEnd: "0.83em",
+        marginInlineStart: "0px",
+        marginInlineEnd: "0px",
+        fontWeight: "bold"
+    }
+
+    const button_set = {
+        display: "flex",
+        gap: "10px",
+        marginTop: "24px",      
+      }
+
+    const delete_button = {
+        backgroundColor: "#fff",
+        border: "2px solid red",
+        borderRadius: "8px",
+        cursor: "pointer",
+        height: "40px",
+        width: "50%"
+    }
+
+    const complete_button = {
+        backgroundColor: "#fff",
+        border: "2px solid green",
+        borderRadius: "8px",
+        cursor: "pointer",
+        height: "40px",
+        width: "50%"
+    }
+
+    return (
+        <div style = {list_wrapper}>
+            <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div>
+            {/* <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div>
+            <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div>
+            <div style = {to_do_container}>
+                <div>
+                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <div> Test context, Nanti diisi inputan dari context</div>
+                </div>
+                <div style = {button_set}>
+                    <button style = {delete_button}>Delete</button>
+                    <button style = {complete_button}>Done</button>
+                </div>
+            </div> */}
         </div>
     )
 }
@@ -193,7 +316,7 @@ const List = () => {
             <h2 style = {list_title}>Working.. 🔥 </h2>
             <ToDoWorking/>
             <h2 style = {list_title}>Done..! 🎉</h2>
-            {/* <ListComplete/> */}
+            <ToDoComplete/>
         </div>
     )
 }
