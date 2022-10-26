f7ed22ace0b613278ae1052b9044232fd8b6616d
lansilvester <alan.lengkong@gmail.com>
2022-10-26 20:55:54 +0800

    Rapihinn

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,55 @@
-const Layout = {
-    margin: "0 auto",
-    maxWidth: "1200px",
-    minWidth: "800px"
+import { useState } from "react";
+import Header from "../header/Header";
+import AddForm from "../form/Form";
+import List from "../list/List";
+import './style.css';
+
+const Layout = () => {
+    // Deklarasi keadaan awal variabel
+    const [title, setTitle] = useState("");
+    const [context,setContext] = useState("");
+    const [todos, setTodos] = useState([
+        {
+          td_id: 1,
+          title: "Sample Title",
+          context: "Sample Context",
+          is_done: 0
+        },
+      ]);
+
+    const onClickHandlerAdd = (event) => {
+        event.preventDefault() //Mencegah file ter-refresh
+        setTitle("") // Mengosongkan input field title
+        setContext("") // Mengosongkan input field context
+        setTodos([...todos, { td_id: todos.length + 1, title: title, context: context, is_done: 0}]);
+     };
+
+    const onChangeHandlerTitle = (event) => {
+        const isi_handler_title = event.target.value;
+        setTitle(isi_handler_title);
+    }
+
+    const onChangeHandlerContext = (event) => {
+        const isi_handler_context = event.target.value;
+        setContext(isi_handler_context);
+    }
+
+    return (
+        <div className="Layout">
+            <Header />
+            <AddForm 
+                title = {title}
+                context = {context}
+                setTitle={setTitle} 
+                setContext={setContext}
+                onChangeHandlerTitle={onChangeHandlerTitle}
+                onChangeHandlerContext={onChangeHandlerContext}
+                onClickHandlerAdd={onClickHandlerAdd}/>
+            <List 
+                todos = {todos}
+                setTodos = {setTodos}/>
+        </div>
+    )
 }
 
-export default Layout
+export default Layout;
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,51 +1,6 @@
+import './style.css'
 const ToDoComplete = ({todos,setTodos}) => {
-    const list_wrapper = {
-        display: "flex",
-        flexWrap: "wrap",
-        gap: "12px",
-    }
-
-    const to_do_container = {
-        border: "4px solid teal",
-        borderRadius: "12px",
-        padding: "12px 24px 24px",
-        width: "270px"
-    }
-
-    const to_do_title = {
-        display: "block",
-        fontSize: "1.5em",
-        marginBlockStart: "0.83em",
-        marginBlockEnd: "0.83em",
-        marginInlineStart: "0px",
-        marginInlineEnd: "0px",
-        fontWeight: "bold"
-    }
-
-    const button_set = {
-        display: "flex",
-        gap: "10px",
-        marginTop: "24px",      
-    }
-
-    const delete_button = {
-        backgroundColor: "#fff",
-        border: "2px solid red",
-        borderRadius: "8px",
-        cursor: "pointer",
-        height: "40px",
-        width: "50%"
-    }
-
-    const complete_button = {
-        backgroundColor: "#fff",
-        border: "2px solid green",
-        borderRadius: "8px",
-        cursor: "pointer",
-        height: "40px",
-        width: "50%"
-    }
-
+   
     // function onClickHandlerDelete(id){
     //     setTodos(current => current.filter(todo => {
     //         return todo.td_id !== id
@@ -58,7 +13,7 @@ const ToDoComplete = ({todos,setTodos}) => {
     // };
 
         return (
-        <div style = {list_wrapper}>
+        <div className="list_wrapper">
         {/* //     {todos.map((todo) => (
         //     <div style = {to_do_container} key={todo.td_id}>
         //         <div>
@@ -72,14 +27,14 @@ const ToDoComplete = ({todos,setTodos}) => {
         //         </div>
         //     </div>
         //     ))} */}
-            <div style = {to_do_container}>
+            <div className="to_do_container">
                 <div>
-                    <h2 style = {to_do_title}> Test todo list, nanti diisi dari inputan title </h2>
+                    <h2 className = "to_do_title"> Test todo list, nanti diisi dari inputan title </h2>
                     <div> Test context, Nanti diisi inputan dari context</div>
                 </div>
-                <div style = {button_set}>
-                    <button style = {delete_button}>Delete</button>
-                    <button style = {complete_button}>Done</button>
+                <div className = "button_set">
+                    <button className = "delete_button">Delete</button>
+                    <button className = "complete_button">Done</button>
                 </div>
             </div>
 
@@ -107,4 +62,47 @@ const ToDoComplete = ({todos,setTodos}) => {
     )
 }
 
-export default ToDoComplete
+const ToDoWorking = ({todos,setTodos}) => { 
+
+    // const onClickHandlerDelete = () => {
+    //     setTodos(current => current.filter(todo => {
+    //         return todo.td_id !== id
+    //     }),
+    //     );
+    //     console.log('tombol hapus')
+    //     console.log(todos)
+    // };
+
+
+    function onClickHandlerDelete(id){
+        setTodos(current => current.filter(todo => {
+            return todo.td_id !== id
+        }),);
+        console.log(todos)
+    }
+
+    function onClickHandlerComplete(id){
+        console.log(todos)
+    };
+    
+    return (
+        <div className = "list_wrapper">
+            {todos.map((todo) => (
+            <div className = "to_do_container" key={todo.td_id}>
+                <div>
+                    <p>{todo.td_id}</p>
+                    <h2 className = "to_do_title">{todo.title}</h2>
+                    <div>{todo.context}</div>
+                </div>
+                <div className = "button_set">
+                    <button className = "delete_button" onClick={() =>onClickHandlerDelete(todo.td_id)} >Delete</button>
+                    <button className = "complete_button" onClick={() => onClickHandlerComplete(todo.td_id)}>Done</button>
+                </div>
+            </div>
+            ))}
+          
+        </div>
+    )
+}
+
+export {ToDoComplete, ToDoWorking}
