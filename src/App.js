c5e5b78d0ba723e2ab6a92eb2f091cd805b17e13
Apriyadi Hegarto <ahegarto@gmail.com>
2022-10-27 19:08:33 +0700

    tambahan untuk test syntax map dan filter

--- src/App.js
+++ src/App.js
@@ -2,33 +2,81 @@ import { useState } from "react";
 
 const App = () => {
     const [ayam,setAyam] = useState([
-    // var ayam = [
         {aid:1, name:'goreng'},
-        {aid:2, name:'bakar'},
-        {aid:3, name:'rebus'},
+        {aid:2, name:'serundeng'},
+        {aid:3, name:'rebus'},  
         {aid:4, name:'gulai'}
     ])
 
-    console.log('sebelum tombol')
-    console.log(ayam)
-    
-    function Handler(){
-        ayam.map((isi_ayam) => {
-            if (isi_ayam.aid === 3){
-                console.log(isi_ayam.name)
-            }return setAyam(ayam);
-        }
-        )
-    }
-    
+    //  /$$$$$$$$       /$$ /$$   /$$    
+    // | $$_____/      | $$|__/  | $$    
+    // | $$        /$$$$$$$ /$$ /$$$$$$  
+    // | $$$$$    /$$__  $$| $$|_  $$_/  
+    // | $$__/   | $$  | $$| $$  | $$    
+    // | $$      | $$  | $$| $$  | $$ /$$
+    // | $$$$$$$$|  $$$$$$$| $$  |  $$$$/
+    // |________/ \_______/|__/   \___/  
 
+    // Edit with map
+    // function Handler(id){
+    //     const sementara = ayam.map((isi_ayam)=>{   
+    //         if (isi_ayam.aid === id) {
+    //             return ({...isi_ayam,name: 'sate'});
+    //         } 
+    //         return isi_ayam;  
+    //     });
+    //     setAyam(sementara)
+    // }   
+    // console.log(ayam)
+
+    // Edit with filter
+    // function Handler(id){
+    //     const sementara = ayam.filter((isi_ayam) =>{
+    //         return isi_ayam.aid !== id
+    //     })
+    //     return setAyam([...sementara, {aid : id, name : 'sate'}]);
+    // }
+    // console.log(ayam)
+
+
+    //  /$$$$$$$            /$$             /$$              
+    // | $$__  $$          | $$            | $$              
+    // | $$  \ $$  /$$$$$$ | $$  /$$$$$$  /$$$$$$    /$$$$$$ 
+    // | $$  | $$ /$$__  $$| $$ /$$__  $$|_  $$_/   /$$__  $$
+    // | $$  | $$| $$$$$$$$| $$| $$$$$$$$  | $$    | $$$$$$$$
+    // | $$  | $$| $$_____/| $$| $$_____/  | $$ /$$| $$_____/
+    // | $$$$$$$/|  $$$$$$$| $$|  $$$$$$$  |  $$$$/|  $$$$$$$
+    // |_______/  \_______/|__/ \_______/   \___/   \_______/
+
+    // Delete with map
+    // function Handler(id){
+    //     const sementara = ayam.map((isi_ayam)=>{   
+    //         if (isi_ayam.aid !== id) {
+    //             return (isi_ayam);
+    //         } 
+    //         // return null;  
+    //     });
+    //     setAyam(sementara)
+    // }   
+    // console.log(ayam)
+
+    // Delete with filter
+    function Handler(id){
+        const sementara = ayam.filter((isi_ayam) =>{
+            return isi_ayam.aid !== id
+        })
+        setAyam(sementara);
+    }
+    console.log(ayam)
 
 
     return (
         <div>
-            <button onClick={() =>Handler()}>
+            <button onClick={() =>Handler(3)}>
                 Satein ayamnya
             </button>
+            {/* To check rendering */}
+            <p>ayam dibakar jadi {ayam[2]['name']}</p>
         </div>
     )
 }
